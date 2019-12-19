acda64ede25a0d6850310b5864c077a3b570e781
Andreas Maier <a.maier@amitc.de>
2019-12-19 11:36:29 +0100

    added kubdernetes ips

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 mvn clean install
-docker build -t localhost:5000/msa-stock-test:version1.0 .
-docker push localhost:5000/msa-stock-test:version1.0
+docker build -t localhost:5000/stock:version2.0 .
+docker push localhost:5000/stock:version2.0
+kubectl apply -f stock-deployment.yaml
+#kubectl apply -f stock-service.yaml
